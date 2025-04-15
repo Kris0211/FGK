1d2993c35bdcfc4848a1bafa7ffb6833f9db9f0b
Filip <93220072+toLLto@users.noreply.github.com>
2025-04-15 13:29:22 +0200

    wip: Merge happened

--- main.cpp
+++ main.cpp
@@ -33,11 +33,11 @@ int main(int argc, char** argv)
 	std::shared_ptr<Scene> scene = std::make_shared<Scene>(2);
 
 	// Spheres
-	rtx::Sphere s1(rtx::Vector3(-0.5f, 0.5f, -3.f), 0.33f);
-	Material sm1(Color(Color::GRAY), 128.f, 5.f, 2.f, 0.f);
+	rtx::Sphere s1(rtx::Vector3(-0.5f, 0.5f, -2.f), 0.33f);
+	Material sm1(Color(Color::GRAY), 128.f, 5.f, 2.f, -4.f);
 	std::shared_ptr<SphereRenderable> sphere1 = std::make_shared<SphereRenderable>(sm1, s1);
 	
-	rtx::Sphere s2(rtx::Vector3(0.5f, 0.5f, -6.f), 0.33f);
+	rtx::Sphere s2(rtx::Vector3(0.5f, 0.5f, 0.f), 0.33f);
 	Material sm2(Color(Color::GRAY), 128.f, 20.f, 0.f, 1.f);
 	std::shared_ptr<SphereRenderable> sphere2 = std::make_shared<SphereRenderable>(sm2, s2);
 
@@ -45,28 +45,28 @@ int main(int argc, char** argv)
 	scene->AddRenderable(sphere2);
 
 	// Planes
-	rtx::Plane p1(rtx::Vector3(4.f, 0.f, 0.f), rtx::Vector3(-1.f, 0.f, 0.f));
-	Material pm1(Color(Color::ORANGE), 128.f, 1.f, 0.f, 0.f);
+	rtx::Plane p1(rtx::Vector3(4.f, 0.f, 0.f), rtx::Vector3(1.f, 0.f, 0.f));
+	Material pm1(Color(Color::RED), 128.f, 1.f, 0.f, 0.f);
 	std::shared_ptr<PlaneRenderable> plane1 = std::make_shared<PlaneRenderable>(pm1, p1);
 	
-	rtx::Plane p2(rtx::Vector3(-4.f, 0.f, 0.f), rtx::Vector3(1.f, 0.f, 0.f));
-	Material pm2(Color(Color::LIME), 128.f, 1.f, 0.f, 0.f);
+	rtx::Plane p2(rtx::Vector3(-4.f, 0.f, 0.f), rtx::Vector3(-1.f, 0.f, 0.f));
+	Material pm2(Color(Color::GREEN), 128.f, 1.f, 0.f, 0.f);
 	std::shared_ptr<PlaneRenderable> plane2 = std::make_shared<PlaneRenderable>(pm2, p2);
 
-	rtx::Plane p3(rtx::Vector3(0.f, 4.f, 0.f), rtx::Vector3(0.f, -1.f, 0.f));
-	Material pm3(Color(Color::ROSE), 128.f, 1.f, 0.f, 0.f);
+	rtx::Plane p3(rtx::Vector3(0.f, 4.f, 0.f), rtx::Vector3(0.f, 1.f, 0.f));
+	Material pm3(Color(Color::BLUE), 128.f, 1.f, 0.f, 0.f);
 	std::shared_ptr<PlaneRenderable> plane3 = std::make_shared<PlaneRenderable>(pm3, p3);
 
-	rtx::Plane p4(rtx::Vector3(0.f, -4.f, 0.f), rtx::Vector3(0.f, 1.f, 0.f));
-	Material pm4(Color(Color::VIOLET), 128.f, 1.f, 0.f, 0.f);
+	rtx::Plane p4(rtx::Vector3(0.f, -4.f, 0.f), rtx::Vector3(0.f, -1.f, 0.f));
+	Material pm4(Color(Color::CYAN), 128.f, 1.f, 0.f, 0.f);
 	std::shared_ptr<PlaneRenderable> plane4 = std::make_shared<PlaneRenderable>(pm4, p4);
 
-	rtx::Plane p5(rtx::Vector3(0.f, 0.f, 16.f), rtx::Vector3(0.f, 0.f, -1.f));
-	Material pm5(Color(Color::AZURE), 128.f, 1.f, 0.f, 0.f);
+	rtx::Plane p5(rtx::Vector3(0.f, 0.f, 8.f), rtx::Vector3(0.f, 0.f, 1.f));
+	Material pm5(Color(Color::YELLOW), 128.f, 1.f, 0.f, 0.f);
 	std::shared_ptr<PlaneRenderable> plane5 = std::make_shared<PlaneRenderable>(pm5, p5);
 
-	rtx::Plane p6(rtx::Vector3(0.f, 0.f, -16.f), rtx::Vector3(0.f, 0.f, 1.f));
-	Material pm6(Color(Color::SPRING_GREEN), 128.f, 1.f, 0.f, 0.f);
+	rtx::Plane p6(rtx::Vector3(0.f, 0.f, -8.f), rtx::Vector3(0.f, 0.f, -1.f));
+	Material pm6(Color(Color::MAGENTA), 128.f, 1.f, 0.f, 0.f);
 	std::shared_ptr<PlaneRenderable> plane6 = std::make_shared<PlaneRenderable>(pm6, p6);
 	
 	scene->AddRenderable(plane1);
@@ -77,7 +77,7 @@ int main(int argc, char** argv)
 	scene->AddRenderable(plane6);
 
 	// Lights
-	PointLight light1(rtx::Vector3(0.f, 0.5f, -8.f), 
+	PointLight light1(rtx::Vector3(0.f, 0.f, 0.f), 
 		LightIntensity(1.f, 1.f, 1.f), 1.f, 0.05f, 0.01f);
 	std::shared_ptr<Light> pl1 = std::make_shared<PointLight>(light1);
 	scene->AddLight(pl1);
@@ -87,7 +87,7 @@ int main(int argc, char** argv)
 		WIDTH, HEIGHT, rtx::Vector3(0, 0, -10.f), rtx::Vector3(0, 0, 1.f));
 
 	std::shared_ptr<Camera> perspCamera = std::make_shared<PerspectiveCamera>(
-		WIDTH, HEIGHT, 100.f, 90.f, rtx::Vector3(0, 0, -10.f), rtx::Vector3(0, 0, 1.f));
+		WIDTH, HEIGHT, 100.f, 90.f, rtx::Vector3(0, 0, -5.f), rtx::Vector3(0, 0, 1.f));
 
 	std::cout << "Rendering images... Please wait.\n";
 
--- src/PointLight.cpp
+++ src/PointLight.cpp
@@ -3,9 +3,9 @@
 rtx::Vector3 PointLight::CalculateLightColor(std::vector<std::shared_ptr<Renderable>> renderables,
 	rtx::Vector3 intersectionPoint, std::shared_ptr<Renderable> closestRenderable, rtx::Vector3 cameraDir, int n)
 {
-    rtx::Vector3 baseColor = closestObject->GetMaterial().GetColor().ToVector();
-    float specCoeff = closestObject->GetMaterial().specularCoeff;
-    float specIntensity = closestObject->GetMaterial().specular;
+    rtx::Vector3 baseColor = closestRenderable->GetMaterial().GetColor().ToVector();
+    float specCoeff = closestRenderable->GetMaterial().specularCoeff;
+    float specIntensity = closestRenderable->GetMaterial().specular;
 
     rtx::Vector3 lightColorVec(intensity.r, intensity.g, intensity.b);
     rtx::Vector3 ambient = baseColor * 0.2f * lightColorVec;
@@ -13,18 +13,18 @@ rtx::Vector3 PointLight::CalculateLightColor(std::vector<std::shared_ptr<Rendera
     bool isInShadow = false;
 
     rtx::Vector3 offsetIntersection = intersectionPoint +
-        (intersectionPoint - closestObject->GetPosition()).Normal() * 0.001f;
+        (intersectionPoint - closestRenderable->GetPosition()).Normal() * 0.001f;
     rtx::Ray shadowRay(offsetIntersection, (position - offsetIntersection).Normal(),
         (position - offsetIntersection).Length());
 
-    for (int i = 0; i < objects.size(); i++) 
+    for (int i = 0; i < renderables.size(); i++) 
     {
         if (i == n) 
             continue;
 
         rtx::Vector3 shadowIntersection;
         Material tempMat;
-        if (objects[i]->Trace(shadowRay, shadowIntersection, tempMat)) 
+        if (renderables[i]->Trace(shadowRay, shadowIntersection, tempMat)) 
         {
             isInShadow = true;
             break;
@@ -38,7 +38,7 @@ rtx::Vector3 PointLight::CalculateLightColor(std::vector<std::shared_ptr<Rendera
 
     rtx::Vector3 lightDir = (position - intersectionPoint).Normal();
     const float lightDist = (position - intersectionPoint).Length();
-    rtx::Vector3 normal = (intersectionPoint - closestObject->GetPosition()).Normal();
+    rtx::Vector3 normal = (intersectionPoint - closestRenderable->GetPosition()).Normal();
     rtx::Vector3 viewDir = cameraDir.Normal();
 
     float attenuation = 1.0f / (constAtten + linearAtten * lightDist + quadAtten * lightDist * lightDist);
@@ -50,7 +50,7 @@ rtx::Vector3 PointLight::CalculateLightColor(std::vector<std::shared_ptr<Rendera
     reflectDir = reflectDir.Normal();
 
     float RdotV = std::max(reflectDir.Dot(viewDir), 0.0f);
-    float specFactor = pow(RdotV, closestObject->GetMaterial().specularCoeff) * closestObject->GetMaterial().specular;
+    float specFactor = pow(RdotV, closestRenderable->GetMaterial().specularCoeff) * closestRenderable->GetMaterial().specular;
 
     rtx::Vector3 specular = rtx::Vector3(1.0f, 1.0f, 1.0f) * specFactor * lightColorVec;
 
